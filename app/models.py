fd4dffc31729062b0e69554aef4ece21954c4ab9
paraigor <103633539+paraigor@users.noreply.github.com>
2025-01-21 21:49:02 +0300

    Update models

--- app/models.py
+++ app/models.py
@@ -9,7 +9,6 @@ class Storage(models.Model):
     address = models.TextField("Адрес", max_length=200)
     temperature = models.PositiveSmallIntegerField("Температура")
     ceiling = models.FloatField("Высота потолка, м",)
-    dimensions = models.CharField("Размер", max_length=20)
     price = models.DecimalField(
         "Цена аренды",
         max_digits=8,
@@ -20,15 +19,26 @@ class Storage(models.Model):
 
 class Box(models.Model):
     number = models.CharField("Номер", max_length=20)
-    address = models.TextField("Адрес", max_length=200)
+    storage = models.ForeignField(storage)
     floor = models.PositiveSmallIntegerField("Этаж")
-    volume = models.DecimalField(
-        "Объем, м3",
+    length = models.DecimalField(
+        "Длина, м",
         max_digits=4,
         decimal_places=1,
         validators=[MinValueValidator(0)],
     )
-    dimensions = models.CharField("Размер", max_length=20)
+    width = models.DecimalField(
+        "Ширина, м",
+        max_digits=4,
+        decimal_places=1,
+        validators=[MinValueValidator(0)],
+    )
+    height = models.DecimalField(
+        "Высота, м",
+        max_digits=4,
+        decimal_places=1,
+        validators=[MinValueValidator(0)],
+    )
     price = models.DecimalField(
         "Цена аренды",
         max_digits=8,
@@ -40,8 +50,8 @@ class Box(models.Model):
 
 class Order(models.Model):
     STATUSES = [
-        ("accepted", "Принята"),
-        ("ended", "Завершена"),
+        ("accepted", "Принят"),
+        ("ended", "Завершен"),
     ]
     status = models.CharField("Статус записи", max_length=20, choices=STATUSES)
     date = models.DateField("Дата начала аренды")
